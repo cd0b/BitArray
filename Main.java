93bb3c5333f62024d3dad2de09d9e939c8006ce8
Can Deniz <dcb.denizcan@gmail.com>
2020-12-15 13:58:15 +0300

    Simple usage added in Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+import burakcs.bitarray.*;
+import burakcs.debug.*;
+import burakcs.exceptions.*;
+
+
+
+public class Main() {
+
+	public static void main(String[] args) {
+
+		BitArray bitArray = new BitArray(96);
+
+		System.out.println(bitArray);
+
+		System.out.println();
+
+	}
+
+}
